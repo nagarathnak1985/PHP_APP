7571d21dd1c11e45a2ca92664734998cbe0ee202
nagarathnak1985 <knagarathna140@gmail.com>
2026-08-25 13:27:34 +0530

    m4

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
     echo "Welcome to Project deployment via CI/CD using Jenkins123.
-    and i have aplied trigger as SCM Poll.";
+    and i have aplied trigger as SCM Poll.
+    nagarathna
+    ";
 ?>
